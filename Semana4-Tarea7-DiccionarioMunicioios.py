8dbbaf92a3a4e45b064ff6567cde00e036f2bea6
JohnVindel <jev30_vindel@hotmail.com>
2021-07-04 14:30:26 -0600

    primer commit

---
+++ Semana4-Tarea7-DiccionarioMunicioios.py
@@ -0,0 +1,15 @@
+#*****Examen, Ejercicio 5 - Diccionario de Municipios*****#
+
+municipios = {
+    "Cortes": "San Pedro Sula",
+    "Copan": "Santa Rosa de Copan",
+    "Francisco Morazan": "Tegucigalpa",
+    "Intibuca": "La Esperanza",
+    "Comayagua": "Comayagua"
+}
+
+try:
+    intro = input("Ingrese el Municipio: ")
+    print("El Municipio es {} y tiene como cabecera a {}.".format(intro, format(municipios[intro], )))
+except KeyError:
+    print ("El pais que ingreso no existe")
